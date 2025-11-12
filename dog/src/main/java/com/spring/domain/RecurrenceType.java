e6150cd221e7ec8a490a75cbaf686415efb46cc8
coco1150 <coco1150@naver.com>
2025-11-12 14:46:04 +0900

    remove previously committed dog folder

--- dog/src/main/java/com/spring/domain/RecurrenceType.java
+++
@@ -1,8 +0,0 @@
-package com.spring.domain;
-
-public enum RecurrenceType {
-    DAILY,        // 매일
-    WEEKLY,       // 주별 (요일 지정)
-    MONTHLY,      // 매월
-    CUSTOM        // 사용자 지정 (n일/n주 간격)
-}
