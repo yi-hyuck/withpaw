c9bf18509f8837c8596ad0e8bfa45ee47cbc94ba
hyuckjinlee <mhyy020@gmail.com>
2025-11-13 17:31:44 +0900

    Add(sql): Add CRUD Query

---
+++ database/CRUD query.sql
@@ -0,0 +1,35 @@
+-- PET --
+-- 반려동물 정보 등록
+INSERT INTO breed (breedname) VALUES ('말티즈');
+INSERT INTO Pet (user_id, petname, breed_id, birthdate, gender, neuter, weight)
+VALUES (1, '티즈', 1, '2024-10-10', 'F', false, 2.12);
+
+-- FOOD --
+-- 검색어 조회
+SELECT food_id, foodname FROM food
+WHERE foodname LIKE CONCAT('%', '감자', '%');
+
+-- 상세정보 조회
+SELECT foodname, edible, description, cautions FROM food
+WHERE food_id = 1;
+
+-- SYMPTOM --
+-- 증상 목록 조회
+SELECT symptom_id, title, symptomdate FROM symptom
+WHERE pet_id = 1
+ORDER BY symptomdate DESC, symptom_id DESC;
+
+-- 증상 추가
+INSERT INTO symptom (pet_id, title, symptom, symptomdate)
+VALUES (1, '구토 증상', '어제 사료를 먹고 구토를 함', '2025-11-12');
+
+-- 증상 수정
+UPDATE symptom
+SET title = '구토 증상(수정)',
+	symptom = '어제 사료를 먹고 구토를 함(수정)',
+	symptomdate = '2025-11-13'
+WHERE symptom_id = 1;
+
+-- 증상 삭제
+DELETE FROM symptom
+WHERE symptom_id = 1;
