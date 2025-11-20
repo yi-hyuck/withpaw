2e68d8f484afabd26bd6968e5f7ca6cc4665543c
yi-hyuck <mhyy020@gmail.com>
2025-11-20 17:50:16 +0900

    Update(CRUD query.sql): Update 반려동물 정보 조회, 등록, 수정, 계정 삭제

--- database/CRUD query.sql
+++ database/CRUD query.sql
@@ -7,22 +7,28 @@ SET
 WHERE user_id = 변경될 사용자 id값;
 
 -- 계정 삭제
+DELETE FROM pet WHERE user_id = 삭제될 사용자 id값;
 DELETE FROM member WHERE user_id = 삭제될 사용자 id값;
 
 
 -- PET --
--- 반려동물 정보 조회
-SELECT petname, birthdate, breed FROM pet WHERE pet_id = 조회 될 반려동물 id값;
+-- 사용자의 반려동물 모두 조회(이름, 생년월일, 품종)
+SELECT p.petname, p.birthdate, b.breedname FROM Pet p JOIN Breed b ON p.breed_id = b.breed_id WHERE p.user_id = 1;
+
+-- 사용자의 특정 반려동물 상세 조회(이름, 품종명, 생년월일, 성별, 중성화여부, 무게)
+SELECT p.petname, b.breedname, p.birthdate, p.gender, p.neuter, p.weight
+    FROM Pet p JOIN Breed b ON p.breed_id = b.breed_id
+    WHERE p.user_id = 조회할 사용자 id AND p.pet_id = 조회할 반려동물 id;
 
 -- 반려동물 정보 등록
-INSERT INTO pet (user_id, petname, breed, birthdate, gender, neuter, weight)
-VALUES (user_id 값, '반려동물 이름', '품종명', 'yyyy-mm-dd', 'M', false, 몸무게값);
+INSERT INTO Pet (user_id, petname, breed_id, birthdate, gender, neuter, weight)
+VALUES (user_id 값, '반려동물 이름', 'breed_id 값, yyyy-mm-dd', 'M', false, 몸무게값);
 
 -- 반려동물 정보 수정
 UPDATE pet
 SET
 	petname = '수정된 반려동물 이름',
-    breed = '수정된 반려동물 품종명',
+    breed_id = 수정된 반려동물 품종 id,
 	birthdate = 'yyyy-mm-dd',
     gender = 'F',
     neuter = true,
